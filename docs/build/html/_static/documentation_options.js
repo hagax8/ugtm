db36bbf3ead12bf3316c7e3bca6ffcd2790bc4e5
git <git@github.com>
2018-05-13 12:58:29 +0100

    removed docs

--- docs/build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: 'v1.0',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
